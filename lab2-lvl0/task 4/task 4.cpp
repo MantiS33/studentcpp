6f079484cdcdd95cdf2713617d0b43b2d264863d
MantiS33 <bendr2000@gmail.com>
2019-03-25 15:05:56 +0500

    исправления с учетом замечаний

--- lab2-lvl0/task 4/task 4.cpp
+++ lab2-lvl0/task 4/task 4.cpp
@@ -4,23 +4,9 @@ using namespace std;
 
 bool numTest(int Num, bool& Test2, int& Test3, bool& Test4)
 {
-	if ((Num % 2) == 0)
-	{
-		Test2 = true;
-	}
-	else
-	{
-		Test2 = false;
-	}
+	Test2 = (Num % 2 == 0 ? true : false);
 	Test3 = Num % 5;
-	if (Num > 10)
-	{
-		Test4 = true;
-	}
-	else
-	{
-		Test4 = false;
-	}
+	Test4 = (Num > 10 ? true : false);
 	if (abs(Num) == Num)
 	{
 		return true;
@@ -29,56 +15,20 @@ bool numTest(int Num, bool& Test2, int& Test3, bool& Test4)
 	{
 		return false;
 	}
+	
 }
 
 int main()
 {
-	bool Test2, Test4;
+	bool Test1,Test2, Test4;
 	int Test3, Num;
 	cout << "Enter a number: ";
 	cin >> Num;
-	if (numTest(Num, Test2, Test3, Test4) == true)
-	{
-		cout << "Positive number" << endl;
-		if (Test2 == true)
-		{
-			cout << "the number is even" << endl;
-		}
-		else
-		{
-			cout << "The number is not even" << endl;
-		}
-		cout << "the remainder of dividing the number by 5  " << Test3 << endl;
-		if (Test4 == true)
-		{
-			cout << "this number is more than 10" << endl;
-		}
-		else
-		{
-			cout << "this number is not more than 10" << endl;
-		}
-	}
-	else
-	{
-		cout << "negative number" << endl;
-		if (Test2 == true)
-		{
-			cout << "the number is even" << endl;
-		}
-		else
-		{
-			cout << "The number is not even" << endl;
-		}
-		cout << "the remainder of dividing the number by 5  " << Test3 << endl;
-		if (Test4 == true)
-		{
-			cout << "this number is more than 10" << endl;
-		}
-		else
-		{
-			cout << "this number is not more than 10" << endl;
-		}
-	}
+	Test1 = numTest(Num, Test2, Test3, Test4);
+	(Test1 == true ? cout << "the number is positiv" << endl : cout << "the number is negative" << endl);
+	(Test2 == true ? cout << "the number is even" << endl : cout << "the number is not even" << endl);
+	cout << "the remainder of dividing the number by 5  " << Test3 << endl;
+	(Test4 == true ? cout << "the number is more then 10" << endl : cout << "the number is less the 10" << endl);
 	system("pause");
 	return 0;
 }
