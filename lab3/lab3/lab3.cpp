edc0753cf6cb0d4bc67072b89cee59bed56d008c
MantiS33 <bendr2000@gmail.com>
2019-04-08 21:57:59 +0500

    лабораторная №3

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	int num;
+	bool test = false;
+	cin >> num;
+	ifstream in("d://input.txt");
+	if (!in)
+	{
+		cout << "error openning the file" << endl;
+	}
+	while (!in.eof())
+	{
+		int temp;
+		in >> temp;
+		if (temp == num)
+		{
+			test = true;
+		}
+	}
+	in.close();
+	if (test == true)
+	{
+		ofstream out("d://output.txt");
+		out << "this number is in the file" << endl;
+		out.close();
+	}
+	else
+	{
+		ofstream out("d://output.txt");
+		out << "this number is not in the file." << endl;
+		out.close();
+	}
+	system("pause");
+	return 0;
+}
