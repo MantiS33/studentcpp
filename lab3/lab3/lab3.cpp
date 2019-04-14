6f3df88debf57b96f746f4b6dba4e26cc9fc5b6a
MantiS33 <bendr2000@gmail.com>
2019-04-14 16:02:15 +0500

    добавлен список

--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -3,10 +3,68 @@
 #include <fstream>
 using namespace std;
 
+struct node
+{
+	int value;
+	node *next;
+};
+
+node*first = NULL;
+
+void add_last(int value)
+{
+	node*tmp = new node;
+	tmp->value = value;
+	tmp->next = NULL;
+	if (first == NULL)
+	{
+		first = tmp;
+		return;
+	}
+	node*last = first;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = tmp;
+	last = tmp;
+}
+
+void print(int& num)
+{
+	node*tmp = first;
+	while (tmp != NULL)
+	{
+		if (tmp->value == num)
+		{
+			ofstream out("d://output.txt");
+			out << "this number is in the file." << endl;
+			out.close();
+		}
+		else
+		{
+			ofstream out("d://output.txt");
+			out << "this number is not in the file.";
+			out.close();
+		}
+		tmp = tmp->next;
+	}
+}
+
+void remove()
+{
+	while (first != NULL)
+	{
+		node*tmp = first;
+		first = first->next;
+		delete tmp;
+	}
+	first = NULL;
+}
+
 int main()
 {
 	int num;
-	bool test = false;
 	cin >> num;
 	ifstream in("d://input.txt");
 	if (!in)
@@ -17,24 +75,12 @@ int main()
 	{
 		int temp;
 		in >> temp;
-		if (temp == num)
-		{
-			test = true;
-		}
+		add_last(temp);
 	}
 	in.close();
-	if (test == true)
-	{
-		ofstream out("d://output.txt");
-		out << "this number is in the file" << endl;
-		out.close();
-	}
-	else
-	{
-		ofstream out("d://output.txt");
-		out << "this number is not in the file." << endl;
-		out.close();
-	}
+	print(num);
+	cout << endl;
+	remove();
 	system("pause");
 	return 0;
 }
