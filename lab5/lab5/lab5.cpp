19d457f6789d6ae0aef955c66c076d4f1cf3bb8f
MantiS33 <bendr2000@gmail.com>
2019-05-06 18:26:55 +0500

    итоговая версия, но это не точно

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -6,7 +6,7 @@ class rectangle
 private:
 	double x1, y1, x2, y2;
 public:
-	rectangle() /*:x1(0),y1(0),x2(0),y2(0)*/ {};
+	rectangle() :x1(0), y1(0), x2(0), y2(0) {};
 	~rectangle()
 	{
 
@@ -16,7 +16,54 @@ public:
 	void change_place();
 	void change_size();
 	void least_rectangle();
+	void crossing();
 };
+
+double min(double a, double b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+double max(double a, double b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+void rectangle::crossing()
+{
+	rectangle* temp = new rectangle[2];
+	(temp[0]).x1 = x1;
+	(temp[0]).x2 = x2;
+	(temp[0]).y1 = y1;
+	(temp[0]).y2 = y2;
+	cout << "ввидите координаты x левых точек второго прямоугольника: ";
+	cin >> (temp[1]).x1;
+	cout << "ввидите координаты y верхних точек второго прямоугольника: ";
+	cin >> (temp[1]).y1;
+	cout << "ввидите координаты x правых точек второго прямоугольника: ";
+	cin >> (temp[1]).x2;
+	cout << "ввидите координаты y нижних точек второго прямоугольника: ";
+	cin >> (temp[1]).y2;
+	if ((temp[0]).x1 > (temp[1]).x2 || (temp[0]).x2 < (temp[1]).x1 || (temp[0]).y1<(temp[1]).y2 || (temp[0]).y2>(temp[0]).y1)
+	{
+		cout << "прямоугольники не пересекаются" << endl;
+	}
+	else
+	{
+		cout << "координаты прямоугольника" << endl
+			<< "A: (" << max((temp[0]).x1, (temp[1]).x1) << ";" << min((temp[0]).y1, (temp[1]).y1) << ")" << endl
+			<< "B: (" << min((temp[0]).x2, (temp[1]).x2) << ";" << min((temp[0]).y1, (temp[1]).y1) << ")" << endl
+			<< "C: (" << min((temp[0]).x2, (temp[1]).x2) << ";" << max((temp[0]).y2, (temp[1]).y2) << ")" << endl
+			<< "D: (" << max((temp[0]).x1, (temp[1]).x1) << ";" << max((temp[0]).y2, (temp[1]).y2) << ")" << endl;
+	}
+	delete[] temp;
+}
 
 void rectangle::least_rectangle()
 {
@@ -152,6 +199,7 @@ int main()
 		<< "3)изменить расположение " << endl
 		<< "4)изменить размер " << endl
 		<< "5)координаты наименьшего общего прямоугольника " << endl
+		<< "6)Прямоугольник получившийся на пересечении 2 заданных " << endl
 		<< "0)закрыть программу" << endl
 		<< "!)вызвать меню " << endl
 		<< ">";
@@ -185,6 +233,11 @@ int main()
 			a.least_rectangle();
 			break;
 		}
+		case '6':
+		{
+			a.crossing();
+			break;
+		}
 		case '!':
 		{
 			cout << "1)ввести значения " << endl
@@ -192,6 +245,7 @@ int main()
 				<< "3)изменить расположение " << endl
 				<< "4)изменить размер " << endl
 				<< "5)координаты наименьшего общего прямоугольника " << endl
+				<< "6)прямоугольник получившийся при пересечении двух заданных" << endl
 				<< "0)закрыть программу" << endl
 				<< "!)вызвать меню " << endl;
 			break;
