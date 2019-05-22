d595fdd357805851c8988150eb47bcfff5bfe63f
MantiS33 <bendr2000@gmail.com>
2019-05-22 18:06:10 +0500

    наработки курсового проекта, со временем будет дополняться и изменяться(осторожно здесь много глупостей)

---
+++ курс тест/курс тест3/курс тест3.cpp
@@ -0,0 +1,244 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class vector
+{
+private:
+	int x, y;
+public:
+	vector(): x(0),y(0) {}
+	vector(int X,int Y): x(X),y(Y) {}
+	friend ostream& operator<<(ostream& out, vector& v)
+	{
+		out << "(" << v.x << ";" << v.y << ")";
+		return out;
+	}
+};
+
+class test
+{
+private:
+	int num;
+	string str;
+public:
+	test() :num(0), str("nope") {}
+	//test(int num, string str) :num(num), str(str) {}
+	friend ostream& operator<<(ostream& out, const test& elem)
+	{
+		out << elem.num << " : " << elem.str;
+		return out;
+	}
+	friend istream& operator>>(istream& in, test& elem)
+	{
+		in >> elem.num;
+		elem.add();
+		return in;
+	}
+	void add()
+	{
+		char* buff = new char[256];
+		cin.getline(buff, 256);
+		str = buff;
+		delete[] buff;
+	}
+	/*friend istream& operator>>(istream& in, test& elem)
+	{
+		in >> elem.num;
+		in >> elem.str;
+		return in;
+	}*/
+};
+
+
+
+template <class T>
+class list
+{
+private:
+	struct element
+	{
+		T data;
+		element* next;
+	};
+	element* head, * tail;
+	element* temp;
+public:
+	list():head(NULL),tail(NULL),temp(NULL) {}
+	~list()
+	{
+		while (head)
+		{
+			tail = head->next;
+			delete head;
+			head = tail;
+		}
+	}
+
+	friend ostream& operator <<(ostream& out, list& elem)
+	{
+		elem.temp = elem.head;
+		while (elem.temp != NULL)
+		{
+			out << elem.temp->data << "\n";
+			elem.temp = elem.temp->next;
+		}
+		return out;
+	}
+	friend istream& operator>>(istream& in, list& elem)
+	{
+		elem.temp = new element;
+		elem.temp->next = NULL;
+		in >> elem.temp->data;
+		if (elem.head != NULL)
+		{
+			elem.tail->next = elem.temp;
+			elem.tail = elem.temp;
+		}
+		else
+		{
+			elem.head = elem.tail = elem.temp;
+		}
+		return in;
+	}
+	void add(T elem);
+	/*void output();
+	T& Get()
+	{
+		return Temp->data;
+	}*/
+};
+
+template <class T>
+void list<T>::add(T elem)
+{
+	temp = new element;
+	temp->data = elem;
+	temp->next = NULL;
+	if (head != NULL)
+	{
+		tail->next = temp;
+		tail = temp;
+	}
+	else
+	{
+		head = tail = temp;
+	}
+}
+
+/*template<class T>
+void list<T>::output()
+{
+	Temp = head;
+	while (Temp != NULL)
+	{
+		cout << Get() << endl;
+		Temp = Temp->next;
+	}
+}*/
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	list<test> a;
+	list<int> b;
+	list<vector> c;
+	char choise,choisemain;
+	cout << "1)пользовательский тип данных" << endl 
+		<< "2)тип данных int" << endl 
+		<< "3)тип вектор" << endl 
+		<< "0)закрыть" << endl 
+		<< ">";
+	cin >> choisemain;
+	while (choisemain)
+	{
+		switch (choisemain)
+		{
+		case '1':
+		{
+			cout << "1)добавить элемент" << endl << "2)вывести информацию" << endl << "0)закрыть" << endl << ">";
+			cin >> choise;
+			while (choise!='0')
+			{
+				switch (choise)
+				{
+				case '1':
+				{
+					cin >> a;
+					break;
+				}
+				case '2':
+				{
+					cout << a;
+					break;
+				}
+				}
+				cout << ">";
+				cin >> choise;
+			}
+			a.~list();
+			break;
+		}
+		case '2':
+		{
+			cout << "1)добавить элемент" << endl << "2)вывести информацию" << endl << "0)закрыть" << endl << ">";
+			cin >> choise;
+			while (choise!='0')
+			{
+				switch (choise)
+				{
+				case '1':
+				{
+					cin>>b;
+					break;
+				}
+				case '2':
+				{
+					cout<<b;
+					break;
+				}
+				}
+				cout << ">";
+				cin >> choise;
+			}
+			b.~list();
+			break;
+		}
+		case '3':
+		{
+			cout << "1)добавить элемент" << endl << "2)вывести информацию" << endl << "0)закрыть" << endl << ">";
+			cin >> choise;
+			while (choise != '0')
+			{
+				switch (choise)
+				{
+				case '1':
+				{
+					int num, num1;
+					cin >> num;
+					cin >> num1;
+					c.add(vector(num, num1));
+					break;
+				}
+				case '2':
+				{
+					cout << c;
+					break;
+				}
+				}
+				cout << ">";
+				cin >> choise;
+			}
+			c.~list();
+			break;
+		}
+		case '0':
+		{
+			return 0;
+		}
+		}
+		cout << ">";
+		cin >> choisemain;
+	}
+	return 0;
+}
---
+++ курс тест/курс тест4/курс тест4.cpp
@@ -0,0 +1,290 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <Windows.h>
+using namespace std;
+
+class fileinfo
+{
+private:
+	int day, month, year, hour, minute;
+public:
+	fileinfo():day(0),month(0),year(0),hour(0),minute(0) {}
+	fileinfo(int Day,int Month,int Year,int Hour,int Minute):day(Day),month(Month),year(Year),hour(Hour),minute(Minute) {}	
+	bool operator<=(const fileinfo& x)
+	{
+		return (this->day <= x.day) && (this->month <= x.month) && (this->year <= x.year) && (this->hour <= x.hour) && (this->minute <= x.minute);
+	}
+	bool operator !=(const fileinfo & y)
+	{
+		return (!(this->day == y.day)) && (!(this->month == y.month)) && (!(this->year == y.year)) && (!(this->hour == y.hour)) && (!(this->minute == y.minute));
+	}
+	friend ostream& operator<<(ostream& out,const fileinfo& date)
+	{
+		out << date.day << "." << date.month << "." << date.year << "  " << date.hour << ":" << date.minute;
+		return out;
+	}
+};
+
+template<class Tkey, class Tvalue>
+class list
+{
+private:
+	struct element
+	{
+		Tkey data;
+		Tvalue data1;
+		int count=0;
+		element* next;
+	};
+	element* head, * tail, * temp;
+	void add()
+	{
+		if (head != NULL)
+		{
+			tail->next = temp;
+			tail = temp;
+		}
+		else
+		{
+			head = tail = temp;
+		}
+	}
+public:
+	list() :head(NULL), tail(NULL), temp(NULL) {}
+	~list()
+	{
+		while (head)
+		{
+			tail = head->next;
+			delete head;
+			head = tail;
+		}
+	}
+	bool unique()
+	{
+		element* Temp = head;
+		while (Temp != NULL)
+		{
+			if (Temp->data == temp->data)
+				return false;
+			Temp = Temp->next;
+		}
+		return true;
+	}
+	Tvalue& operator[](Tkey key)
+	{
+		this->temp = this->head;
+		while (this->temp != NULL)
+		{
+			if (this->temp->data == key)
+			{
+				this->temp->count += 1;
+				return this->temp->data1;
+			}
+			this->temp = this->temp->next;
+		}
+		throw;
+	}
+	friend ostream& operator<<(ostream & out, list & elem)
+	{
+		elem.temp = elem.head;
+		while (elem.temp != NULL)
+		{
+			out << elem.temp->data << " : " << elem.temp->data1 << "  " <<elem.temp->count << "\n";
+			elem.temp = elem.temp->next;
+		}
+		return out;
+	}
+	friend istream& operator>>(istream & in, list & elem)
+	{
+		elem.temp = new element;
+		elem.temp->next = NULL;
+		in >> elem.temp->data;
+		while (elem.unique() == false)
+		{
+			in.clear();
+			cout << "uncorrectable key!" << endl << "input new: ";
+			in >> elem.temp->data;
+		}
+		in >> elem.temp->data1;
+		elem.add();
+		return in;
+	}
+	void Delete(Tvalue num)
+	{
+		bool test = false;
+		temp = head;
+		while (temp != NULL)
+		{
+			if (temp->data1 <= num)
+			{
+				if (temp == head)
+				{
+					head = head->next;
+					delete temp;
+					test = true;
+					break;
+				}
+				if (temp == tail)
+				{
+					temp = head;
+					while (temp->next != tail)
+					{
+						temp = temp->next;
+					}
+					delete tail;
+					temp->next = NULL;
+					tail = temp;
+					test = true;
+					break;
+				}
+				if (temp != head && temp != tail)
+				{
+					temp = head;
+					while (temp->next->data1 != num)
+					{
+						temp = temp->next;
+					}
+					element* Temp = temp->next;
+					temp->next = temp->next->next;
+					delete Temp;
+					test = false;
+					break;
+				}
+			}
+			temp = temp->next;
+		}
+		if (test == true)
+		{
+			Delete(num);
+		}
+		else
+		{
+			return;
+		}
+	}
+	void add(Tkey key, Tvalue item)
+	{
+		temp = new element;
+		temp->next = NULL;
+		temp->data = key;
+		while (unique() == false)
+		{
+			cout << "uncorrectable key!" << endl << "input new: ";
+			cin >> temp->data;
+		}
+		temp->data1 = item;
+		add();
+	}
+};
+
+char Interpreter(string &str)
+{
+	if (str == "menu")
+	{
+		return '1';
+	}
+	if (str == "dir")
+	{
+		return '2';
+	}
+	if (str == "create")
+	{
+		return '3';
+	}
+	if (str == "delete")
+	{
+		return '4';
+	}
+	if (str == "show")
+	{
+		return '5';
+	}
+	if (str == "createf")
+	{
+		return '6';
+	}
+	if (str == "end")
+	{
+		return '0';
+	}
+	return 'e';
+}
+
+int main()
+{
+	string file;
+	list<string, fileinfo> a;
+	string Choise;
+	cout << "V:\\virtual>";
+	cin >> Choise;
+	cout << endl;
+	char choise;
+	choise = Interpreter(Choise);
+	while (choise)
+	{
+		switch (choise)
+		{
+		case '1':
+		{
+			cout <<endl<< "menu: show menu" << endl 
+				<< "dir: show all file" << endl
+				<< "create: create a file" << endl 
+				<< "createf: create a folder " << endl
+				<< "show: show certain file"<<endl
+				<< "delete: delete certain file" << endl
+				<< "end: close the programm" << endl<<endl;
+			break;
+		}
+		case '2':
+		{
+			cout << a;
+			break;
+		}
+		case '3':
+		{
+			cout << "input name: ";
+			cin >> file;
+			SYSTEMTIME st;
+			GetLocalTime(&st);
+			a.add(file, fileinfo(st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute));
+			break;
+		}
+		case '4':
+		{
+			int day, month, year, hour, minute;
+			cin >> day;
+			cin >> month;
+			cin >> year;
+			cin >> hour;
+			cin >> minute;
+			a.Delete(fileinfo(day, month, year, hour, minute));
+			break;
+		}
+		case '5':
+		{
+			cout << "input a name of file: ";
+			cin >> file;
+			cout << file << " : " << a[file];
+			break;
+		}
+		case '6':
+			cout << "not ready" << endl;
+			break;
+		case 'e':
+		{
+			cout <<endl<< "error! try again" << endl<<endl;
+			break;
+		}
+		case '0':
+			return 0;
+		}
+		cout << endl << "V:\\virtual>";
+		cin >> Choise;
+		cout << endl;
+		choise = Interpreter(Choise);
+	}
+	return 0;
+}
