b0ba371de426f68081889510c84c349de7750e05
MantiS33 <bendr2000@gmail.com>
2020-03-13 22:13:33 +0500

    выполнено задание 2

--- Csharp_lab1/Csharp_lab1/Program.cs
+++ Csharp_lab1/Csharp_lab1/Program.cs
@@ -3,16 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace testing_of_Csharp
 {
+    /*[XmlInclude(typeof(Square))]
+    [XmlInclude(typeof(Rectangle))]
+    [XmlInclude(typeof(Circle))]
+    [XmlInclude(typeof(Ellipsis))]*/
     public abstract class Figure
     {
         public string Figure_Type { get; set; }
         public double Area { get; set; }
-        public int Frame_thickness { get; set; }
+        public uint Frame_thickness { get; set; }
 
         public abstract void Calc_Area();
+    }
+
+    public class Rectangle : Figure
+    {
+        public double A;
+        public double B;
+
+        public Rectangle()
+        {
+            Figure_Type = "rectangle";
+            A = B = 0;
+            Frame_thickness = 0;
+        }
+        public Rectangle(double a,double b)
+        {
+            Figure_Type = "rectangle";
+            A = a;
+            B = b;
+            Frame_thickness = 0;
+            Calc_Area();
+        }
+        public Rectangle(double a,double b,uint frame)
+        {
+            Figure_Type = "rectangle";
+            A = a;
+            B = b;
+            Frame_thickness = frame;
+            Calc_Area();
+        }
+
+        public override void Calc_Area()
+        {
+            Area = (A + Frame_thickness) * (B + Frame_thickness);
+        }
+
+        public override string ToString()
+        {
+            return $"Type of figure:{Figure_Type}; Frame thickness:{Frame_thickness}; Area:{Area};";
+        }
     }
 
     public class Square : Figure
@@ -32,7 +76,7 @@ namespace testing_of_Csharp
             Frame_thickness = 0;
             Calc_Area();
         }
-        public Square(double a, int frame)
+        public Square(double a, uint frame)
         {
             Figure_Type = "square";
             A = a;
@@ -42,7 +86,7 @@ namespace testing_of_Csharp
 
         public override void Calc_Area()
         {
-            Area = A * A + Frame_thickness;
+            Area = (A + Frame_thickness) * (A + Frame_thickness);
         }
 
         public override string ToString()
@@ -68,7 +112,7 @@ namespace testing_of_Csharp
             Radius = radius;
             Calc_Area();
         }
-        public Circle(double radius, int frame)
+        public Circle(double radius, uint frame)
         {
             Figure_Type = "circle";
             Frame_thickness = frame;
@@ -78,7 +122,7 @@ namespace testing_of_Csharp
 
         public override void Calc_Area()
         {
-            Area = 3.14 * Math.Pow(Radius, 2) + Frame_thickness;
+            Area = 3.14 * Math.Pow(Radius + Frame_thickness, 2);
         }
 
         public override string ToString()
@@ -106,7 +150,7 @@ namespace testing_of_Csharp
             Frame_thickness = 0;
             Calc_Area();
         }
-        public Ellipsis(double major_radius, double minor_radius,int frame)
+        public Ellipsis(double major_radius, double minor_radius,uint frame)
         {
             Figure_Type = "ellipsis";
             Major_Radius = major_radius;
@@ -117,7 +161,7 @@ namespace testing_of_Csharp
 
         public override void Calc_Area()
         {
-            Area = 3.14 * Major_Radius * Minor_Radius + Frame_thickness;
+            Area = 3.14 * (Major_Radius + Frame_thickness) * (Minor_Radius + Frame_thickness);
         }
 
         public override string ToString()
@@ -129,18 +173,68 @@ namespace testing_of_Csharp
     public class Graphics_editor
     {
         public List<Figure> Figures { get; } = new List<Figure>();
+        public void Sorting()
+        {
+            Figures.Sort(delegate (Figure x, Figure y)
+            {
+                if (x.Area == y.Area)
+                    return x.Frame_thickness.CompareTo(y.Frame_thickness);
+                else
+                    return x.Area.CompareTo(y.Area);
+            });
+        }
+
     }
 
     class Program
     {
+        void Menu()
+        {
+
+        }
+
         static void Main(string[] args)
         {
-            Square square = new Square(10,2);
-            Circle circle = new Circle(6, 3);
-            Ellipsis ellipsis = new Ellipsis(5, 3, 5);
-            Console.WriteLine(square.ToString());
-            Console.WriteLine(circle.ToString());
-            Console.WriteLine(ellipsis.ToString());
+            Graphics_editor editor = new Graphics_editor();
+
+            editor.Figures.Add(new Square(10,2));
+            editor.Figures.Add(new Circle(5,6));
+            editor.Figures.Add(new Rectangle(10,2,6));
+            editor.Figures.Add(new Rectangle(12, 12, 0));
+            editor.Figures.Add(new Ellipsis(20, 30, 4));
+
+            
+            Console.WriteLine("Choose the function:");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    {
+                        editor.Sorting();
+                        foreach (Figure aFigure in editor.Figures)
+                        {
+                            Console.WriteLine(aFigure.ToString());
+                        }
+                        break;
+                    }
+                case "2":
+                    {
+                        for(int i=0; i<3;i++)
+                        {
+                            Console.WriteLine(editor.Figures[i]);
+                        }
+                        break;
+                    }
+                case "3":
+                    {
+                        for(int i=editor.Figures.Count-1;i>=editor.Figures.Count-2;i--)
+                        {
+                            Console.WriteLine(editor.Figures[i]);
+                        }
+                        break;
+                    }
+            }
+
             Console.ReadKey();
         }
     }
