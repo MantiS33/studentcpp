fc2db62ab6a4843da8ca08b929c1261036a586b8
MantiS33 <bendr2000@gmail.com>
2020-04-20 00:00:06 +0500

    корректировка кода

--- lab_methods/lab_mathods2/Source.c
+++ lab_methods/lab_mathods2/Source.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <malloc.h>
 
-int newton(int n, double *x, double *y, double *c)
+int newton(int n, double* x, double* y, double* c)
 {
 	int i, j, k;
 	double temp1 = 0.0, temp2 = 0.0;
@@ -22,7 +22,7 @@ int newton(int n, double *x, double *y, double *c)
 		{
 			for (k = 0; k <= j - 1; k++)
 			{
-				if (temp2 == 0.0 && k==0)
+				if (temp2 == 0.0 && k == 0)
 				{
 					temp2 = x[i] - x[k];
 				}
@@ -36,7 +36,7 @@ int newton(int n, double *x, double *y, double *c)
 		}
 		for (k = 0; k <= i - 1; k++)
 		{
-			if (temp2 == 0.0 && k==0)
+			if (temp2 == 0.0 && k == 0)
 			{
 				temp2 = x[i] - x[k];
 			}
@@ -52,11 +52,11 @@ int newton(int n, double *x, double *y, double *c)
 	return 1;
 }
 
-void newton_polynomial(int n, double *c,double *x)
+void newtonPolynomial(int n, double* c, double* x)
 {
-	double temp1=0.0, temp2=0.0, X;
+	double temp1 = 0.0, temp2 = 0.0, X;
 	int i, j;
-	FILE *fp;
+	FILE* fp;
 	fp = fopen("result.txt", "w");
 	temp1 = c[0];
 	fprintf(fp, "y(%f)=%f\n", 0.0, temp1);
@@ -86,34 +86,32 @@ void newton_polynomial(int n, double *c,double *x)
 
 int main(void)
 {
-	int n,i,tmp;
-	double *x, *y, *c;
-	FILE *s_x,*s_y;
+	int n, i;
+	double* x, *y, *c;
+	FILE *sFileX, *sFileY;
 
 	n = 11;
 	x = (double*)malloc(n * sizeof(double));
 	y = (double*)malloc(n * sizeof(double));
 	c = (double*)malloc(n * sizeof(double));
 
-	if ((s_x = fopen("x.txt", "r"))==NULL)
+	if ((sFileX = fopen("x.txt", "r")) == NULL)
 	{
 		printf("can't open the file");
 		getchar();
 		return 0;
 	}
-	if ((s_y = fopen("y.txt", "r")) == NULL)
+	if ((sFileY = fopen("y.txt", "r")) == NULL)
 	{
 		printf("can't open the file");
 		getchar();
 		return 0;
 	}
-	
+
 	for (i = 0; i < n; i++)
 	{
-		fscanf(s_x, "%d", &tmp);
-		x[i] = (double)tmp;
-		fscanf(s_y, "%d", &tmp);
-		y[i] = (double)tmp;
+		fscanf(sFileX, "%lf", &x[i]);
+		fscanf(sFileY, "%lf", &y[i]);
 	}
 
 	if (newton(n, x, y, c) == 0)
@@ -121,23 +119,23 @@ int main(void)
 		free(x);
 		free(y);
 		free(c);
-		fclose(s_x);
-		fclose(s_y);
+		fclose(sFileX);
+		fclose(sFileY);
 		getchar();
 		return 0;
 	}
 
-	newton_polynomial(n, c, x);
+	newtonPolynomial(n, c, x);
 	for (i = 0; i < n; i++)
 	{
-		printf("c[%d]=%f\n",i ,c[i]);
+		printf("c[%d]=%f\n", i, c[i]);
 	}
 
 	free(x);
 	free(y);
 	free(c);
-	fclose(s_x);
-	fclose(s_y);
+	fclose(sFileX);
+	fclose(sFileY);
 	getchar();
 	return 0;
 }
